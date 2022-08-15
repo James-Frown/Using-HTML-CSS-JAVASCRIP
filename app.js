420c66bce6f637b7fe7e10732aac5bafc9e2e576
James-Frown <21122@ctca.co.za>
2022-08-15 15:08:48 +0200

    additional changes to make a basic calculator
    js for button fucntions and jc ifstatements in an array, very basic but comng along... looking to use this experimentation to inform user cart systems for product  orders

--- app.js
+++ app.js
@@ -27,3 +27,67 @@ function displayInput(textVar, target) {
     let i = textVar.value;
     target.innerHTML = i;
 }
+
+let sumArr = [];
+
+function addNums(sum, target) {
+    let i = sum.value;
+    target.innerHTML += i + " + ";
+    sumArr.push(i);
+    sumArr.push("+");
+    sum.value = 0;
+}
+
+function subtractNums(sum, target) {
+    let i = sum.value;
+    target.innerHTML += i + " - ";
+    sumArr.push(i);
+    sumArr.push("-");
+    sum.value = 0;
+}
+
+function multiplyNums(sum, target) {
+    let i = sum.value;
+    target.innerHTML += i + " * ";
+    sumArr.push(i);
+    sumArr.push("*");
+    sum.value = 0;
+}
+
+function divideNums(sum, target) {
+    let i = sum.value;
+    target.innerHTML += i + " / ";
+    sumArr.push(i);
+    sumArr.push("/");
+    sum.value = 0;
+}
+
+function displaySum(sum, target) {
+    let i = sum.value;
+    let out = 0;
+    target.innerHTML += i + " = ";
+    sumArr.push(i);
+    for (let x = 0; x < sumArr.length; x++) {
+        if (sumArr[x] == "+") {
+            out = parseInt(sumArr[x - 1]) + parseInt(sumArr[x + 1]);
+        }
+        if (sumArr[x] == "-") {
+            out = parseInt(sumArr[x - 1]) - parseInt(sumArr[x + 1]);
+        }
+        if (sumArr[x] == "*") {
+            out = parseInt(sumArr[x - 1]) * parseInt(sumArr[x + 1]);
+        }
+        if (sumArr[x] == "/") {
+            out = parseInt(sumArr[x - 1]) / parseInt(sumArr[x + 1]);
+        }
+    }
+    //out = parseInt(sumArr[0]) + parseInt(sumArr[2]);
+    target.innerHTML += out;
+    sum.value = 0;
+}
+
+function resetsSum(target) {
+    sumArr = [];
+    target.innerHTML = "";
+    console.log(target.innerHTML);
+}
